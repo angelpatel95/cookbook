17bc6fc224e3fc0c50b71eb92cd25a53961b0aa4
angelpatel95 <68925132+angelpatel95@users.noreply.github.com>
2021-03-08 14:15:31 -0600

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.urandom(32)
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:pinkpurple@127.0.0.1:5432/cookbook?sslmode=require'
+    SQLALCHEMY_DATABASE_URI = 'postgres://pafohykylwfsvl:0b0dae8f00a068dd9ea854638782c6c472a01ed03907ae3296d1fe6e976f1c27@ec2-54-242-43-231.compute-1.amazonaws.com:5432/dcc9306sv3fdi2'
 
 class ProductionConfig(Config):
     DEBUG = False
